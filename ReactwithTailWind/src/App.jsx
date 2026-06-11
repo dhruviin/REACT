3e0a84ff6d9c66d20a4c3df53ad5b6c9cb532a4a
dhruvin <dhruvinmavani3813@gmail.com>
2026-06-11 12:24:56 +0530

    react tailwind setup and props

---
+++ ReactwithTailWind/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Card1 from "./card1.jsx";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-500 text-white p-4">hello</h1>
+      <Card1 name="John" />
+    </>
+  );
+}
+
+export default App;
